a940ba88c013e0d450d66d8b78dc991cc0f4c721
anitahx <anitaizzy4@gmail.com>
2022-06-02 13:50:26 -0700

    size in bytes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,23 +1,22 @@
 #include <stdio.h>
 /**
-*main - "a C program that prints the size of various files"
+*main - entry
 *Descripton: "prints the size of various files"
 *Return: 0
 */
 int main(void)
 {
+int v;
+long int w;
+long long int x;
+char y;
+float z;
 
-char charType;
-int intType;
-long longType
-long long longlongType;
-float floatType;
-
-printf("Size of a char: %lu byte(s)\n", sizeof(charType));
-printf("Size of an int: %lu byte(s)\n", sizeof(intType));
-printf("Size of a long int: %lu byte(s)\n", sizeof(longType));
-printf("Size of a long long int: %lu byte(s)\n", sizeof(longlongType));
-printf("Size of a float: %lu byte(s)\n", sizeof(floatType));
+printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(y));
+printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(v));
+printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(w));
+printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(y));
+printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(z));
 return (0);
 
 }
