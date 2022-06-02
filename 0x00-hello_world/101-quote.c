37d169417c788adbb19b40442d38eff396725db2
anitahx <anitaizzy4@gmail.com>
2022-06-02 14:46:32 -0700

    a simple operating system, but you have to be a genius to understand the simplicity

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+#include <unistd.h>
+
+/**
+ *main - prints it as is
+ *Description: Prints it exactky as is
+ *Return: Always 0 (Success)
+ *
+*/
+
+int main(void)
+
+{i
+
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+
+	return (1);
+
+}
+
+
