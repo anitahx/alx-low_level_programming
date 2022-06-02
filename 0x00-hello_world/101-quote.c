e4e802422b8217eaf4229d9c76f7808a97977393
anitahx <anitaizzy4@gmail.com>
2022-06-02 14:49:43 -0700

    edits

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,15 +3,16 @@
 #include <unistd.h>
 
 /**
- *main - prints it as is
- *Description: Prints it exactky as is
- *Return: Always 0 (Success)
  *
-*/
+ *  * main - Entry point
+ *
+ *   * Return: Always 0 (Success)
+ *
+ *    */
 
 int main(void)
 
-{i
+{
 
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 
