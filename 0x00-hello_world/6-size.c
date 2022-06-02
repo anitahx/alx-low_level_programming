9cf42a0b7967c187d14752dbf73843c62164e8f0
anitahx <anitaizzy4@gmail.com>
2022-06-02 13:06:06 -0700

    corrections for 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- *main - "a C program that prints the size of various files"
+*main - "a C program that prints the size of various files"
 *Descripton: "prints the size of various files"
 *Return: 0
 */
@@ -13,11 +13,11 @@ long longType
 long long longlongType;
 float floatType;
 
-printf("Size of a char: %ld byte(s)\n", sizeof(charType));
-printf("Size of an int: %ld byte(s)\n", sizeof(intType));
-printf("Size of a long int: %ld byte(s)\n", sizeof(longType));
-printf("Size of a long long int: %ld byte(s)\n", sizeof(longlongType));
-printf("Size of a float: %ld byte(s)\n", sizeof(floatType));
+printf("Size of a char: %lu byte(s)\n", sizeof(charType));
+printf("Size of an int: %lu byte(s)\n", sizeof(intType));
+printf("Size of a long int: %lu byte(s)\n", sizeof(longType));
+printf("Size of a long long int: %lu byte(s)\n", sizeof(longlongType));
+printf("Size of a float: %lu byte(s)\n", sizeof(floatType));
 return (0);
 
 }
