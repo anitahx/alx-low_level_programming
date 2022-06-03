71bb2f8123c9a742829f39d7d768ff4c3c985ee6
anitahx <anitaizzy4@gmail.com>
2022-06-02 22:46:09 -0700

    prints exactly as is

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,17 +1,13 @@
 #include <unistd.h>
 
-/*
- *
- * main - Entry point
- * Description: Prints it exactly as is
- * Return: Always 0 (Success)
- *
+/**
+ *main - Entry point
+ *Description: "prints exactly as is"
+ *Return: Always 0 (Success)
  */
-
 int main(void)
 
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-
 	return (1);
 }
