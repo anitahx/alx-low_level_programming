f2d42b503d323b6080ec1c0a9c1b52aaad561cf1
anitahx <anitaizzy4@gmail.com>
2022-06-02 23:20:56 -0700

    prints variables as they are given

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,7 +7,6 @@
 int main(void)
 
 {
-
 int v;
 long int w;
 long long int x;
@@ -17,7 +16,7 @@ float z;
 printf("Size of a char: %zu byte(s)\n", sizeof(y));
 printf("Size of an int: %zu byte(s)\n", sizeof(v));
 printf("Size of a long int: %zu byte(s)\n", sizeof(w));
-printf("Size of a long long int: %zu byte(s)\n", sizeof(y));
+printf("Size of a long long int: %zu byte(s)\n", sizeof(x));
 printf("Size of a float: %zu byte(s)\n", sizeof(z));
 return (0);
 
