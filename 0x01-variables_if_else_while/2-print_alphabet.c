57e6dd0b4a6ea4b38f1ea5626bca1bb4e6ef7f7e
anitahx <anitaizzy4@gmail.com>
2022-06-03 12:26:08 -0700

    changes to file

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -12,11 +12,11 @@
 int main(void)
 {
 char alphabet;
-for (alphabet = 'A'; alphabet <= 'Z'; alphabet++)
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 {
 putchar(alphabet);
 }
-for (alphabet = 'A'; alphabet <= 'Z'; alphabet++)
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 {
 putchar(alphabet);
 }
