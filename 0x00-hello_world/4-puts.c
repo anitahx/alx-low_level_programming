2bd03699dcbb2cdfc8d8eba4987fac2168e727a6
anitahx <anitaizzy4@gmail.com>
2022-06-05 04:58:48 -0700

    prints a string that inputs the functions

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
-*main - prints a string that in pups the functions
+*main - prints a string that inputs the functions
 *Description: Printing "Programming is like building a multilingual puzzle"
 *Return: Always 0 (Success)
 */
