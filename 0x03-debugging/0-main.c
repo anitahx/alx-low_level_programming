da9482536c71776658dbe12a6bee8ba6f478316a
anitahx <anitaizzy4@gmail.com>
2022-06-08 08:20:22 -0700

    ives the correct output when given a case of 0.

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -7,10 +7,10 @@ void positive_or_negative(int i);
 
 int main(void)
 {
-        int i;
+int i;
 
-        i = 0;
-        positive_or_negative(i);
+i = 0;
+positive_or_negative(i);
 
-        return (0);
+return (0);
 }
