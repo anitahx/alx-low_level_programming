6656e6b4de3b5902a5cd146268bcb523b8c82526
anitahx <anitaizzy4@gmail.com>
2022-06-09 06:56:12 -0700

    a function that prints the numbers, from 0 to 9, followed by a new line.

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -6,7 +6,7 @@
  * Return: void
  */
 
-void print_numbers(void);
+void print_numbers(void)
 {
 
 	char j;
@@ -14,7 +14,7 @@ void print_numbers(void);
 	j = '0';
 	while (j <= '9')
 	{
-		_putchar(j);
+		_putchar(j + '0');
 		j++;
 	}
 	_putchar('\n');
--- 0x04-more_functions_nested_loops/_putchar.c
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -1,7 +1,9 @@
 #include <unistd.h>
 
 /**
- * _putchar - writes characters
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
