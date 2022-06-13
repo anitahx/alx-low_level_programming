e194f1a32895d4902fab2d4cd131c36aac182a5c
anitahx <anitaizzy4@gmail.com>
2022-06-13 06:39:42 -0700

    printing strings

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *_puts - prints a string in reverse
+ *@str: value to be evaluate.
+ *Return: null.
+ */
+void _puts(char *str)
+{
+	int l = 0;
+
+	while (*(str + l) != '\0')
+	{
+		_putchar(str[l]);
+		l++;
+	}
+	_putchar('\n');
+}
