cd3f6b62d7314a1729bb03bdf9b0764766288fd1
anitahx <anitaizzy4@gmail.com>
2022-06-15 03:36:32 -0700

    my files

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,5 @@
+#include "main.h"
+
+/**
+ * _strncat - concertenates two strings
+ *
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,6 +1,7 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
+int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
@@ -14,3 +15,4 @@ void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
 
+#endif
