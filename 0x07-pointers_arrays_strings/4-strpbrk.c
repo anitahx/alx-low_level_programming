ed0b1f30dc45aa9cacf9802a1e5f20d8358cfb7e
anitahx <anitaizzy4@gmail.com>
2022-06-20 08:06:47 -0700

    more mandetory tasks

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,10 +1,14 @@
 #include "main.h"
+#include <string.h>
 
 /**
  * _strpbrk - locates the first occurrence in the string s of any of the bytes in the string
- * Return: a pointer to the byte in s
+ * @s: input value
+ * Return: a pointer to the memory area s
  */
 
 char *_strpbrk(char *s, char *accept)
 
-
+{
+return (strpbrk(s, accept));
+}
--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -2,7 +2,21 @@
 
 /**
  * print_chessboard - prints chessboard
- * Return: 
+ * @a: 2d array of chars
+ * Return: nothing
  */
 
 void print_chessboard(char (*a)[8])
+{
+	int b, c;
+
+	for (b = 0; b < 8; b++)
+	{
+		for (c = 0; c < 8; c++)
+		{
+			_putchar(a[b][c]);
+		}
+
+	_putchar('\n');
+	}
+}
