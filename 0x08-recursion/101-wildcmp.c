f98bd79f2c0ad69bcf435a3ca71a93aff14d5c42
anitahx <anitaizzy4@gmail.com>
2022-06-22 11:13:44 -0700

    a function that compares two strings

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -10,6 +10,7 @@
 int wildcmp(char *str1, char *str2)
 {
 	if (*str1 == '\0' && *str2 == '\0')
+	{
 		return (1);
 	}
 	else if (*str1 == '\0' && *str2 == '*')
@@ -19,11 +20,11 @@ int wildcmp(char *str1, char *str2)
 	else if (*str1 == '\0' || *str2 == '\0')
 	{
 		return (0);
-		}
+	}
 	if (*str1 == *str2)
-		{
+	{
 		return (wildcmp((str1 + 1), (str2 + 1)));
-		}
+	}
 	if (*str2 == '*')
 	{
 	if (*(str2 + 1) == '*')
@@ -34,10 +35,11 @@ int wildcmp(char *str1, char *str2)
 	{
 		return (1);
 	}
-		else
+	else
 	{
 		return (wildcmp(str1 + 1, str2));
-}
+	}
 }
 		return (0);
 }
+
