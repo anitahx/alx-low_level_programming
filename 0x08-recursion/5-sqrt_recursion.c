db14be9de65587e8917609525ae4030af95966fa
anitahx <anitaizzy4@gmail.com>
2022-06-22 06:46:11 -0700

    squares and prime numbers

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,20 +1,39 @@
 #include "main.h"
 
 /**
- * _sqrt_recursion - returns the natural square root of a number
- * @n: number to consider
- * Return: natural square root of a number
- */
+ * sqtRecursive - computes square root recursively doing binary search
+ * @start: inital number
+ * @end: last number within the limit of number
+ * @n: given number
+ * Return: 1 if not found sqrroot, else sqrroot
+ **/
+int sqtRecursive(int start, int end, int n)
+{
+	long mid;
 
-int _sqrt_recursion(int n)
+	if (end >= start)
+	{
+		mid = start + (end - start) / 2;
+		if (mid * mid == n)
+			return (mid);
+/* following binary search */
+		if (mid * mid > n)
+			return (sqtRecursive(start, mid - 1, n));
+		if (mid * mid < n)
+			return (sqtRecursive(mid + 1, end, n));
+	}
+	return (-1);
+}
+/**
+ * _sqrt_recursion - finds the natural square root of a number
+ * @n: given number
+ * Return: square root of n or -1
+ **/
+	int _sqrt_recursion(int n)
 {
 	if (n < 0)
-	{
 		return (-1);
-	}
-
-		if (n > 0)
-		{
-			return (n / _sqrt_recursion(n - 1));
-		}
+	if (n == 0 || n == 1)
+		return (n);
+	return (sqtRecursive(2, n, n));
 }
--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,3 +1,45 @@
 #include "main.h"
 
+/**
+ * sqtRecursive - computes square root recursively
+ * @n: given number
+ * @k: comparison number
+ * Return: 1 if not found sqrroot, else sqrroot
+ **/
+
+int sqtRecursive(int n, int k)
+{
+	if (n <= 0)
+		return (-1);
+	if (n * n == k)
+		return (n);
+	return (sqtRecursive(n - 1, k));
+}
+
+/**
+ * _sqrt_recursion - finds the natural square root of a number
+ * @n: given number
+ * Return: square root of n or -1
+ **/
+
+int _sqrt_recursion(int n)
+{
+	if (n == 1)
+		return (1);
+	return (sqtRecursive(n / 2, n));
+}
+
+/**
+ * is_prime_number - checks if a given number is prime
+ * @n: given number
+ * Return: 1 if number is prime else 0
+ **/
+
 int is_prime_number(int n)
+{
+	if (n <= 1 || _sqrt_recursion(n) >= 1)
+		return (0);
+	if (_sqrt_recursion(n) == -1)
+		return (1);
+	return (_sqrt_recursion(n));
+}
