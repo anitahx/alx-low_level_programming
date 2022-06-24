7d0fbf83c5d5e31496460645f5792589b6f18f4f
anitahx <anitaizzy4@gmail.com>
2022-06-24 10:25:59 -0700

    a program that multiplies two numbers

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -18,11 +18,4 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		return (1);
 	}
-
-/* atoi convert a string to an int */
-	a = atoi(argv[1]);
-	b = atoi(argv[2]);
-	result = a * b;
-	printf("%d\n", result);
-	return (0);
 }
