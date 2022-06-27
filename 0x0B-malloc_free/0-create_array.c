276a4bd6dadc491521d13ace9d2680100c2f9303
anitahx <anitaizzy4@gmail.com>
2022-06-27 04:01:15 -0700

    my project files

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * *create_array - creates an array of chars and initialises it with a specific char
+ * @c: input value
+ * Return: Null if size is 0 or a pointer to the array
+ */
+
+char *create_array(unsigned int size, char c);
+
+
+
---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,9 @@
+#include "main.h"
+
+/**
+ * *_strdup - returns a pointer to a new string which is the string's duplicate
+ * @*str: input value
+ * Return: NULL if the string is NULL
+ */
+
+char *_strdup(char *str);
