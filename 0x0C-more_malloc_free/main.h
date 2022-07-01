de4d34cfb303c2e2b09e80f0168cbf70d7a47af5
anitahx <anitaizzy4@gmail.com>
2022-06-30 23:39:30 -0700

    a program that multiplies two positive numbers

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -7,5 +7,12 @@ char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
+int find_len(char *str);
+char *create_xarray(int size);
+char *iterate_zeroes(char *str);
+void get_prod(char *prod, char *mult, int digit, int zeroes);
+void add_nums(char *final_prod, char *next_prod, int next_len);
+int main(int argc, char *argv[]);
 
 #endif
+
