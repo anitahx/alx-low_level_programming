61cda3818fb655682d148882e9625c33be3dbf94
anitahx <anitaizzy4@gmail.com>
2022-07-03 23:10:03 -0700

    a program that prints the name of the file it was compiled from, followed by a new line

---
+++ 0x0D-preprocessor/main.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - print file name
+ * Return: always 0
+ */
+
+int main(void)
+
+{
+	charx[] = "file_name";
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(x[i]);
+	}
+	putchar('\n');
+	return (0);
+}
