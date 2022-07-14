bfc5893b239af1b8da77894fc1b3f0637fa8cc51
anitahx <anitaizzy4@gmail.com>
2022-07-14 04:03:55 -0700

    task 0 corrections

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include "lists.h"
 #include <stdlib.h>
-
 /**
  * print_list - prints all elements of a list_t list
  * @h: singly linked list to print
@@ -9,19 +8,12 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t times = 0;
+	size_t i;
 
-	while (h != NULL)
+	for (i = 0; h; i++)
 	{
-		if (h->str == NULL)
-			printf("[0] (nil)\n");
-
-		else
-			printf("[%d] %s\n", h->len, h->str);
-
-		times++;
+		printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
 	}
-
-	return (times);
+	return (i);
 }
