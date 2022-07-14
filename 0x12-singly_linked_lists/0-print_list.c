3b72af600d088e49a5a426608ec3ba5c182b485c
anitahx <anitaizzy4@gmail.com>
2022-07-14 03:56:47 -0700

    edits to task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,12 +9,19 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t i;
+	size_t times = 0;
 
-	for (i = 0; h; i++)
+	while (h != NULL)
 	{
-		printf("[%d] %s\n", h->len, h->str);
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+
+		else
+			printf("[%d] %s\n", h->len, h->str);
+
+		times++;
 		h = h->next;
 	}
-	return (i);
+
+	return (times);
 }
