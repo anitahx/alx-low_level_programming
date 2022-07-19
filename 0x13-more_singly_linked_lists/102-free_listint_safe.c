f151ef937fbba5579f3081a6c0477b9a0051ad8c
anitahx <anitaizzy4@gmail.com>
2022-07-18 22:21:55 -0700

    task 13 a function that finds the loop in a linked list

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,39 +1,61 @@
 #include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
- * delete_nodeint_at_index - Deletes the node at a given
- * index of a list
- * @head: A pointer to the address of the
- *        head of the list.
- * @index: The index of the node to be deleted.
- *
- * Return: On success - 1.
- *         On failure - -1.
+ * * _ra - reallocates memory for an array of pointers
+ * to the nodes in a linked list
+ * @list: the old list to append
+ * @size: size of the new list (always one more than the old list)
+ * @new: new node to add to the list
+ * Return: pointer to the new list
  */
-int delete_nodeint_at_index(listint_t **head, unsigned int index)
+
+listint_t **_ra(listint_t **list, size_t size, listint_t *new)
 {
-listint_t *newNode, *tmp = *head;
-unsigned int node;
-if (tmp == NULL)
+listint_t **newlist;
+size_t i;
+newlist = malloc(size * sizeof(listint_t *));
+if (newlist == NULL)
 {
-return (-1);
+free(list);
+exit(98);
 }
-if (index == 0)
-{
-*head = (*head)->next;
-free(tmp);
-return (1);
+for (i = 0; i < size - 1; i++)
+newlist[i] = list[i];
+newlist[i] = new;
+free(list);
+return (newlist);
 }
-for (node = 0; node < (index - 1); node++)
+/**
+ * free_listint_safe - frees a listint_t linked list.
+ * @head: double pointer to the start of the list
+ * Return: the number of nodes in the list
+ */
+size_t free_listint_safe(listint_t **head)
 {
-if (tmp->next == NULL)
+size_t i, num = 0;
+listint_t **list = NULL;
+listint_t *next;
+if (head == NULL || *head == NULL)
+return (num);
+while (*head != NULL)
 {
-return (-1);
+for (i = 0; i < num; i++)
+{
+if (*head == list[i])
+{
+*head = NULL;
+free(list);
+return (num);
 }
-tmp = tmp->next;
 }
-newNode = tmp->next;
-tmp->next = newNode->next;
-free(newNode);
-return (1);
+num++;
+list = _ra(list, num, *head);
+next = (*head)->next;
+free(*head);
+*head = next;
+}
+free(list);
+return (num);
 }
