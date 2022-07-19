3de0d64d77ee7d82f4b88917718c5b9ba6ab4140
anitahx <anitaizzy4@gmail.com>
2022-07-18 19:45:30 -0700

    task 0 output file

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,7 +1,6 @@
-#ifndef _LISTS.H_
-#define _LISTS.H_
+#ifndef LISTS_H
+#define LISTS_H
 #include <stddef.h>
-
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -10,15 +9,11 @@
  * Description: singly linked list node structure
  * for Holberton project
  */
-
 typedef struct listint_s
-
 {
 	int n;
 	struct listint_s *next;
 }	listint_t;
-
-int _putchar(char c);
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
@@ -28,7 +23,8 @@ void free_listint2(listint_t **head);
 int pop_listint(listint_t **head);
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
 int sum_listint(listint_t *head);
-listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
+listint_t *insert_nodeint_at_index
+(listint_t **head, unsigned int index, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
 listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
