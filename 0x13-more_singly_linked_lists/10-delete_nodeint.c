c17b78e151107973618deebcf22c90b6df973660
anitahx <anitaizzy4@gmail.com>
2022-07-18 20:28:43 -0700

    advanced tasks

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,34 +1,40 @@
 #include "lists.h"
-#include <stdlib.h>
-#include <stdio.h>
+
 /**
- * delete_nodeint_at_index - deletes the node at index 
- * @head: head of the list
- * @index: emplacement of the node to delet
- * Return: pointr to head of list
+ * delete_nodeint_at_index - deletes a node in a linked list at a certain index
+ * @head: pointer to the first element in the list
+ * @index: index of the node to delete
+ *
+ * Return: 1 (Success), or -1 (Fail)
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-	unsigned int i;
-	listint_t *current, *subsequent;
+	listint_t *temp = *head;
+	listint_t *current = NULL;
+	unsigned int i = 0;
 
-	if (!head || !*head)
+	if (*head == NULL)
 		return (-1);
-	current = *head;
+
 	if (index == 0)
 	{
 		*head = (*head)->next;
-		free(current);
+		free(temp);
 		return (1);
 	}
-	for (i = 0; i < (index - 1); i++)
+
+	while (i < index - 1)
 	{
-		current = current->next;
-		if (current == NULL)
+		if (!temp || !(temp->next))
 			return (-1);
+		temp = temp->next;
+		i++;
 	}
-	subsequent = current->next;
-	current->next = subsequent->next;
-	free(subsequent);
+
+
+	current = temp->next;
+	temp->next = current->next;
+	free(current);
+
 	return (1);
 }
--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,83 +1,40 @@
 #include "lists.h"
 
-size_t looped_listint_count(listint_t *head);
-size_t free_listint_safe(listint_t **h);
-
 /**
- * looped_listint_count - Counts the number of unique nodes
- * in a linked list.
- * @head: pointer to the head of the listint_t to check.
+ * free_listint_safe - frees a linked list
+ * @h: pointer to the first node in the linked list
  *
- * Return: 0 if the list is not looped through.
- * else - the number of unique nodes in the list.
- */
-size_t looped_listint_count(listint_t *head)
-{
-listint_t *i, *j;
-size_t nodes = 1;
-if (head == NULL || head->next == NULL)
-return (0);
-i = head->next;
-j = (head->next)->next;
-while (j)
-{
-if (i == j)
-{
-i = head;
-while (i != j)
-{
-nodes++;
-i = i->next;
-j = j->next;
-}
-i = i->next;
-while (i != j)
-{
-nodes++;
-i = i->next;
-}
-return (nodes);
-}
-i = i->next;
-j = (j->next)->next;
-}
-return (0);
-}
-
-/**
- * free_listint_safe - Frees a listint_t list safely (ie.
- *                     can free lists containing loops)
- * @h: A pointer to the address of
- *     the head of the listint_t list.
- *
- * Return: The size of the list that was freed.
- *
- * Description: The function sets the head to NULL.
+ * Return: number of elements in the freed list
  */
 size_t free_listint_safe(listint_t **h)
 {
-listint_t *tmp;
-size_t nodes, index;
-nodes = looped_listint_count(*h);
-if (nodes == 0)
-{
-for (; h != NULL && *h != NULL; nodes++)
-{
-tmp = (*h)->next;
-free(*h);
-*h = tmp;
-}
-}
-else
-{
-for (index = 0; index < nodes; index++)
-{
-tmp = (*h)->next;
-free(*h);
-*h = tmp;
-}
-*h = NULL;
-}
-h = NULL;
-return (nodes);
+	size_t len = 0;
+	int diff;
+	listint_t *temp;
+
+	if (!h || !*h)
+		return (0);
+
+	while (*h)
+	{
+		diff = *h - (*h)->next;
+		if (diff > 0)
+		{
+			temp = (*h)->next;
+			free(*h);
+			*h = temp;
+			len++;
+		}
+		else
+		{
+			free(*h);
+			*h = NULL;
+			len++;
+			break;
+		}
+	}
+
+	*h = NULL;
+
+	return (len);
 }
