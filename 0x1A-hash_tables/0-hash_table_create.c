f8ad194d797284cf089217727383c4262737b19e
anitahx <anitaizzy4@gmail.com>
2022-09-23 01:45:24 -0700

    edits

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,24 +1,23 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_create - creates a hash table
- * @size: the size of the hash table
- *
- * Return: pointer to hash table type
- */
+ * hash_table_create - create a hash table
+ * @size: size of the table
+ * Return: pointer to the table
+ **/
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *ht;
 
-	if (!size)
+	if (size == 0)
+	return (NULL);
+	ht = malloc(sizeof(hash_table_t));
+	if (ht == NULL)
 		return (NULL);
 
-	ht = calloc(1, sizeof(hash_table_t));
-	if (!ht)
-		return (NULL);
 	ht->size = size;
 	ht->array = calloc(size, sizeof(hash_node_t *));
-	if (!ht->array)
+	if (ht->array == NULL)
 	{
 		free(ht);
 		return (NULL);
--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,26 +1,31 @@
 #include "hash_tables.h"
+
 /**
- *hash_table_delete - function that deletes a hash table
+ * hash_table_delete - deletes a hash table
+ * @ht: pointer to hash table
  *
- *@ht: is the hash table
+ * Return: void.
  */
 void hash_table_delete(hash_table_t *ht)
 {
-hash_node_t *node, *temp;
-unsigned long int cont = 0;
+	unsigned long i = 0;
+	hash_node_t *node, *temp;
 
-for (cont = 0; cont < ht->size; cont++)
-{
-node = ht->array[cont];
-while (node != NULL)
-{
-temp = node;
-node = node->next;
-free(temp->key);
-free(temp->value);
-free(temp);
-}
-}
-free(ht->array);
-free(ht);
+	if (!ht)
+		return;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		node = ht->array[i];
+		while (node)
+		{
+			temp = node;
+			node = node->next;
+			free(temp->key);
+			free(temp->value);
+			free(temp);
+		}
+	}
+	free(ht->array);
+	free(ht);
 }
