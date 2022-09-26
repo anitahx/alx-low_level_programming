e5c262e2757c9956f0124dcc9fd1ae0d594e50eb
anitahx <anitaizzy4@gmail.com>
2022-09-26 07:46:31 -0700

    more files

---
+++ 0x18-dynamic_libraries/arithmetic.h
@@ -0,0 +1,15 @@
+#ifndef _ARITHMETIC_H_
+#define _ARITHMETIC_H_
+#include <stdio.h>
+
+{
+
+	int getSum(int num1, int num2)
+	int getDifference(int num1, int num2)
+	int getProduct(int num1, int num2)
+	float getQuotient(int num1, int num2)
+	int getModulo(int num1, int num2)
+
+#endif
+}
+
