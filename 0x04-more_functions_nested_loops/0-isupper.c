6dac162800a06cf3b153a463a89e0f2670811061
anitahx <anitaizzy4@gmail.com>
2022-12-29 23:15:48 -0800

    is upper new file

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -7,5 +7,13 @@
  **/
 int _isupper(int c)
 {
-	return (c >= 'A' && c <= 'Z');
+	/*return (c >= 'A' && c <= 'Z');*/
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+	       return (0);
+	}
 }
